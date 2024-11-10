fa1b21127699c4c4374701a275ad81b92cd8e024
dalleth-martinss <essi_dalleth@outlook.com>
2024-11-10 00:53:36 -0300

    commit initial project online store

---
+++ src/main/java/br/com/loja/virtual/LojaVirtualDmApplication.java
@@ -0,0 +1,13 @@
+package br.com.loja.virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualDmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualDmApplication.class, args);
+	}
+
+}
